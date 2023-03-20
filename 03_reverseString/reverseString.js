98c58c503736a35f2734d02f02bae33242358ea7
luke1thompson <lt574@nau.edu>
2023-03-20 08:41:44 -0700

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let backString = "";
+    let i = string.length - 1;
+    for (i; i >= 0; i--) {
+        backString += string.charAt(i);
+    }
 
+    return backString;
 };
 
 // Do not edit below this line
