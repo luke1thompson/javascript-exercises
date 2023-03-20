d4c151370cba69cc55738b8d499d151b6b896638
luke1thompson <lt574@nau.edu>
2023-03-20 12:10:21 -0700

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempf) {
+  let tempc = (tempf - 32) * (5/9);
+  return Number(tempc.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempc) {
+  let tempf = (tempc*(9/5) + 32);
+  return Number(tempf.toFixed(1)); 
 };
 
 // Do not edit below this line
