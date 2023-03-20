b016a26f64d6786411d4de1a4dceb5d143441fa2
luke1thompson <lt574@nau.edu>
2023-03-20 11:26:43 -0700

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    let outputArray = [];
 
-};
+    for (let item of array) {
+        let i = 1;
+        let match = false;
+
+        for (i; i < arguments.length; i++) {
+            //console.log(`testing item ${item} for match with ${arguments[i]}`);
+            if (item === arguments[i]) {
+                match = true;
+                //console.log('Match found!')
+            }
+        }
+        if (!match) {
+            outputArray.push(item);
+        }
+    }
+
+    return outputArray;
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
