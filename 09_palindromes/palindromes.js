bc77b0992dc461f614c489874f7867ce5088dfd2
luke1thompson <lt574@nau.edu>
2023-04-04 14:03:36 -0700

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const clean = cleanString(input);
+    const length = clean.length;
+    const range = Math.floor(length / 2);
+    
+    for (let i = 0; i < range; i++) {
+        if(clean[i] != clean[length - i - 1]) {
+            return false;
+        }
+    }
 
+    return true;
 };
+
+const cleanString = function (input) {
+    let output = "";
+    for (let i = 0; i < input.length; i++) {
+        if (input[i].match(/[a-zA-Z]/)) {
+            output += input[i];
+        }
+    }
+    return output.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
